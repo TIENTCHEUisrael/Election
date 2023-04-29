4fee83c6baf01d6a3397dc80ea0c9ee2b41eae7b
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-04-29 08:20:28 +0200

    Generation of controller Api and simple

---
+++ BackEnd/tilbackelection/app/Http/Controllers/REST/BulletinController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\REST;
+
+use App\Http\Controllers\Controller;
+use App\Models\Bulletin;
+use Illuminate\Http\Request;
+
+class BulletinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bulletin  $bulletin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bulletin $bulletin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bulletin  $bulletin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bulletin $bulletin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bulletin  $bulletin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bulletin $bulletin)
+    {
+        //
+    }
+}
---
+++ BackEnd/tilbackelection/app/Http/Controllers/REST/VotenController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\REST;
+
+use App\Http\Controllers\Controller;
+use App\Models\Vote;
+use Illuminate\Http\Request;
+
+class VotenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vote $vote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vote $vote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vote  $vote
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vote $vote)
+    {
+        //
+    }
+}
